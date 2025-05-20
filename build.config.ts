ffe3f9ef30d98b591423b71521f8e641d9649812
Julien Ripouteau <julien@ripouteau.com>
2025-05-20 02:32:20 +0200

    chore: simplify export paths

--- build.config.ts
+++ build.config.ts
@@ -4,8 +4,5 @@ export default defineBuildConfig({
   entries: ['src/index'],
   declaration: true,
   clean: true,
-  rollup: {
-    emitCJS: true,
-  },
   externals: ['unocss', 'mini-svg-data-uri', '@unocss/preset-mini', '@unocss/preset-mini/utils'],
 })
